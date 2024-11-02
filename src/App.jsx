0b6cf90b43db9d4c85c02886b34dccde459ee9e1
malson89s <malin.sonneblom@hotmail.se>
2024-11-02 13:08:30 +0100

    clean install

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import Brands from './components/Brands'
+import Faq from './components/Faq'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import More from './components/More'
+import Slider from './components/Slider'
+import Subscribe from './components/Subscribe'
+import Testimonials from './components/Testimonials'
 
 function App() {
 
 
   return (
     <>
+      <Header />
+      <Hero />
+      <Brands />
+      <Features />
+      <Slider />
+      <More />
+      <Testimonials />
+      <Faq />
+      <Subscribe />
+      <Footer />
 
     </>
   )
